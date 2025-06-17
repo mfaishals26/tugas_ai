8ad549a39e66b1085226d298a6d061bf8b17ed4e
mfaishals26 <161547997+mfaishals26@users.noreply.github.com>
2025-06-17 09:33:15 +0700

    Update app.py

--- app.py
+++ app.py
@@ -69,7 +69,7 @@ quotes = {
 
 # ========== CHATBOT (LIVE CURHAT) ========== #
 def chat_with_bot(user_message):
-    API_URL = "https://api-inference.huggingface.co/models/microsoft/DialoGPT-medium"
+    api_url = "https://api-inference.huggingface.co/models/microsoft/DialoGPT-small"
     headers = {
         "Authorization": "Bearer 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"  
     }
